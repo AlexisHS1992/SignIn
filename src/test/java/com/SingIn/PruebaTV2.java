85893f325d53d67cf7434e03dd2cb3ad4c2934e8
AlexisHS1992 <alexisherrerasalazar9293@gmail.com>
2021-01-20 11:33:23 -0500

    Version2

--- src/test/java/com/SingIn/PruebaTV2.java
+++ src/test/java/com/SingIn/PruebaTV2.java
@@ -1,10 +1,14 @@
 package com.SingIn;
 
-import org.junit.After;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+//import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PruebaTV2 {
@@ -14,8 +18,8 @@ public class PruebaTV2 {
 	// para habilitar el check
 	// true = se selecciona el check
 	// false = no selecciona el check
-	public boolean SelectSignUpEnable = true;
-	public boolean OffersEnable = true;
+	public boolean SelectAdressEnable = true;
+	public boolean SelectTermsEnable = true;
 	
 	
 	@Before
@@ -31,14 +35,14 @@ public void setUp() {
 	
 	@Test
 	
-	public void testNewEXperience( ) {
+	public void setloginusuario() throws InterruptedException, IOException {
 		
 		driver.findElement(By.className("login")).click();
 		driver.findElement(By.id("email")).sendKeys("alexisherrerasalazar9293@gmail.com");
 		driver.findElement(By.id("passwd")).sendKeys("123456");
 		driver.findElement(By.id("SubmitLogin")).click();
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,10 +50,62 @@ public void setUp() {
 		String pageHeading = driver.findElement(By.className("page-heading")).getText();
 		System.out.println("Page Heading: " +pageHeading);
 	
+		WebElement optionsSearch = driver.findElement(By.id("search_query_top"));
+		optionsSearch.sendKeys("blouse");
+		WebElement buttonsearch = driver.findElement(By.xpath("//button[@class=\"btn btn-default button-search\"]"));
+		buttonsearch.click();
+		
+		driver.findElement(By.id("search")).click();
+		driver.findElement(By.id("color_8")).click();
+		
+		WebElement buttonimage = driver.findElement(By.xpath("//img[@class=\"replace-2x img-responsive\"]"));
+		buttonimage.click();
+		WebElement button_AddCard = driver.findElement(By.xpath("//button[@class=\"exclusive\"]"));
+		button_AddCard.click();
+		
+		//WebElement buttonCross = driver.findElement(By.xpath("//span[@class=\"cross\"]"));
+		//buttonCross.click();
+		
+		//driver.findElement(By.className("cross")).click();
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.get("http://automationpractice.com/index.php?controller=order");
+		
+		
+		WebElement buttonSummary = driver.findElement(By.xpath("//a[@class=\"button btn btn-default standard-checkout button-medium\"]"));
+		buttonSummary.click();
+					
+
+		WebElement CheckAddress = driver.findElement(By.id("addressesAreEquals"));
+		if(SelectAdressEnable) CheckAddress.click();
+		
+		WebElement button_Address = driver.findElement(By.xpath("//button[@class=\"button btn btn-default button-medium\"]"));
+		button_Address.click();
+		
+		WebElement Check_Terms = driver.findElement(By.id("cgv"));
+		if(SelectTermsEnable) Check_Terms.click();
+		
+		WebElement button_Shipping = driver.findElement(By.xpath("//button[@class=\"button btn btn-default standard-checkout button-medium\"]"));
+		button_Shipping.click();
+		
+		WebElement button_PayBank = driver.findElement(By.xpath("//a[@class=\"bankwire\"]"));
+		button_PayBank.click();
+		
+		WebElement button_ConfirmOrder = driver.findElement(By.xpath("//button[@class=\"button btn btn-default button-medium\"]"));
+		button_ConfirmOrder.click();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
 	}
-	@After
-	private void tearDown() {
+
+	//@After
+	//public void tearDown() {
 		// TODO Auto-generated method stub
-		driver.quit();
-			}
+		//driver.quit();
+			//}
 }
